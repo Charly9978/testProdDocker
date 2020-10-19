7309a8f7136b614865547095340f5664e08807fd
benj <diebenjamin@yahoo.com>
2020-10-19 13:25:14 +0200

    initialisation

---
+++ backend/app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '88ce2144354ce9b727f33271da516265'),
+    },
+  },
+});
---
+++ backend/app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '75750489-aa8a-4fd4-af77-72213f26f796'
+};
