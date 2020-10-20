16b462d8247b2f2a5ea1bf2dab645741774e3f69
benj <diebenjamin@yahoo.com>
2020-10-20 22:14:02 +0200

    ajout url dans config strapi

--- backend/app/config/server.js
+++ backend/app/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: "https://api.pdpbenj.site",
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '88ce2144354ce9b727f33271da516265'),
